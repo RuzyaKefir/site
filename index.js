941b926e797558e14b0ec8bc0a6d9e5dd1ee31b4
Ruzya <149572602+RuzyaKefir@users.noreply.github.com>
2023-11-01 12:55:09 +0300

    я еврейка

--- index.js
+++ index.js
@@ -7,25 +7,43 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
-
+// это короче что бы хранить открытый блок
 var currentlyOpenBlock = null;
 
+
 function toggleBlock(blockId) {
+    // тут эта хуета неведомая рот ее ебал гетает айди кнопки которую нажимают пон да
     var blockToToggle = document.getElementById(blockId);
+
+    // це провiрочка на то есть ли блок вообще
     if (blockToToggle) {
+        // если другой блок уже открыт то у него нахуй удаляется класс блок опен что бы не выебывалось хуйло
         if (currentlyOpenBlock) {
-            currentlyOpenBlock.style.display = 'none';
+            currentlyOpenBlock.classList.remove('block-open');
         }
-        blockToToggle.style.display = 'block';
+
+        // эт надо что бы показать блок какой он красивый а кодер молодец
+        blockToToggle.classList.add('block-open');
+
+        // это обновление ранее объявленной переменной что бы хранить в ней открытый блок
         currentlyOpenBlock = blockToToggle;
     }
 }
 
+// це для кнопо4ек бек в блоках
 function hideBlock(blockId) {
+    // тут эта хуета неведомая рот ее ебал гетает айди кнопки которую нажимают пон да
     var blockToHide = document.getElementById(blockId);
+
+    // це провiрочка на то есть ли блок вообще
     if (blockToHide) {
-        blockToHide.style.display = 'none';
+        // тут у него нахуй удаляется класс блок опен что бы не выебывалось хуйло
+        blockToHide.classList.remove('block-open');
+
+        // тут сброс переменной в ноль нахуй патамушта панабирают далбаебав
         currentlyOpenBlock = null;
     }
 }
+
+
 
